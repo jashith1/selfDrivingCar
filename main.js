580e4f42c1bb98269e37bf2e51726bacf5ffe4ce
bloppai <bloppai@bloppai.attlocal.net>
2023-07-18 19:31:11 -0500

    draw roads

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const canvas = document.getElementById('myCanvas');
 canvas.width = 200;
 
+const lanes = 3;
+
 const ctx = canvas.getContext('2d');
-const road = new Road(canvas.width / 2, canvas.width * 0.9, 3);
-const car = new Car(100, 700, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9, lanes);
+const car = new Car(road.getLaneCenter(Math.floor(lanes / 2)), 700, 30, 50);
 car.draw(ctx);
 
 animate();
@@ -11,7 +13,13 @@ animate();
 function animate() {
 	car.update();
 	canvas.height = window.innerHeight;
+
+	ctx.save();
+	ctx.translate(0, -car.y + canvas.height * 0.7);
+
 	road.draw(ctx);
 	car.draw(ctx);
+
+	ctx.restore();
 	requestAnimationFrame(animate);
 }
